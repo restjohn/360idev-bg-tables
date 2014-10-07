e655f6f9258510a2f3b197d245cdecb3221a96cb
Robert St. John <stjohnr@bit-sys.com>
2014-10-07 15:28:34 -0600

    pushing to github

--- Tables/ViewController.m
+++ Tables/ViewController.m
@@ -18,12 +18,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _flowers = [[NSMutableArray alloc] initWithObjects:@"Daliah", @"Lily", @"Rose", @"Petunia", nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger) numberOfSectionsInTableView: (UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_flowers count];
+}
+
+- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString* CellIdentifier = @"theCell";
+    UITableViewCell* theCell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (theCell == nil)
+    {
+        theCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    theCell.textLabel.text = [_flowers objectAtIndex:indexPath.row];
+    return theCell;
 }
 
 @end
